75ceeff4040e23cdb7b5cb28831411d15d992589
aszajko <aszajko25@gmail.com>
2021-05-15 12:19:57 +0200

    add my project

---
+++ FinalProject/src/main/java/com/asz/FinalProject/ProjectApplicationRun.java
@@ -0,0 +1,13 @@
+package com.asz.FinalProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApplicationRun {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApplicationRun.class, args);
+	}
+
+}
