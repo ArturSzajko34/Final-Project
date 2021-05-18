ba40a2b428aa7f1e7cf2a8fa3b0ab5a7f3ccc55f
aszajko <aszajko25@gmail.com>
2021-05-18 21:16:57 +0200

    Create a table and relationship

--- FinalProject/src/main/java/com/asz/FinalProject/ProjectApplicationRun.java
+++ FinalProject/src/main/java/com/asz/FinalProject/ProjectApplicationRun.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProjectApplicationRun {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjectApplicationRun.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectApplicationRun.class, args);
+    }
 
 }
